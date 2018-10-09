859c3f7f4712012e054292e15e6fcfcf08b521a9
Yauheni_Sakalou <ysakalou@slb.com>
2018-10-09 22:39:27 +0300

    Add project files.

---
+++ CoreHelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CoreHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter your name");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+            Console.ReadKey();
+        }
+    }
+}
